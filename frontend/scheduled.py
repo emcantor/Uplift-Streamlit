af6645cda6934583761060e8ba687b7cdbbd555a
Nicolas Esnis <nicolas.esnis@gmail.com>
2022-03-11 14:57:57 -0800

    few edits

--- frontend/scheduled.py
+++ frontend/scheduled.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import streamlit as st
 
-df_id = pd.read_csv('../scheduled.txt', sep='/n', header=None, engine='python', index_col=0)
-df_list = pd.read_json('../uplifts_data.json', orient='index')
-df = pd.merge(df_id, df_list, left_index=True, right_index=True)
+df_id = pd.read_csv('../scheduled.txt', sep='/n', names=['index'])
+df_list = pd.read_json('../uplifts_data.json',
+                       orient='index', convert_axes=False).reset_index()
+df_list['index'] = df_list['index'].astype(int)
+df = pd.merge(df_id, df_list, how='inner', on='index')
 df_write = df[['bundle_a', 'bundle_i', 'campaign_ids']]
-df_write
+
 
 def scheduled():
     st.header('Scheduled Uplifts')
     st.write(df_write)
+    # st.write(df_id)
+    # st.write(df_list)
